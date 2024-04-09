3b210023e028c82dbebab603b0bf8009deca98bb
Guilherme Almeida <98991759+mixiricagui@users.noreply.github.com>
2024-04-08 22:16:59 -0300

    1

---
+++ front.js
@@ -0,0 +1,29 @@
+
+function SaibaMais(){
+    // JOGO QUE ESTA NO "DESTAQUE PAGINA"
+}
+
+function JogoDestaque(){
+
+    
+}
+
+function item1(){
+    // JOGO QUE ESTA NO "ITEM 1"
+}
+
+function item2(){
+    // JOGO QUE ESTA NO "ITEM 2"
+}
+
+function LinckInstagram(){
+    window.open('https://www.instagram.com/mixirica_gui/');
+}
+
+function LinckPlayStory(){
+    window.open('https://play.google.com/store/apps/dev?id=7531183554578649479');
+}
+
+function LoneWolf(){
+    window.open('https://play.google.com/store/apps/details?id=com.Guilherme.TARCKS');
+}
