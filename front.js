4d34b603ed3714951701edc138b4ad439d7b8293
Guilherme Almeida <98991759+mixiricagui@users.noreply.github.com>
2024-08-15 21:17:24 -0300

    dia 15/08

--- front.js
+++ front.js
@@ -32,3 +32,41 @@ function LoneWolf(){
 function Carrossel()
 {
 }
+
+/*SLIDER TARCKS*/
+const slider = document.querySelectorAll('.slider')
+const btnPrev = document.getElementById('prev-button')
+const btnNext = document.getElementById('next-button')
+
+let currentSlider = 0;
+
+function hideSlider(){
+    slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider(){
+    slider[currentSlider].classList.add('on')
+}
+
+function nextSlider(){
+    hideSlider()
+    if(currentSlider == slider.length -1){
+        currentSlider = 0
+    }else{
+        currentSlider++
+    }
+    showSlider()
+}
+
+function prevSlider(){
+    hideSlider()
+    if(currentSlider == 0){
+        currentSlider = slider.length -1
+    }else{
+        currentSlider--
+    }
+    showSlider()
+}
+
+btnNext.addEventListener('click', prevSlider)
+btnPrev.addEventListener('click', nextSlider)
